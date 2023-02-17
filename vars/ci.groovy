9173efbe9a559e1e1d4c1094bbf521ee151d584d
Hari <harishsarma.v@gmail.com>
2023-02-17 19:44:12 +0530

    updating roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -30,12 +30,12 @@ def call() {
             SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
           }
           steps {
-//            wrap ([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-//                println "Password = ${SONAR_PASS}"
-            sh "sonar-scanner -Dsonar.host.url=http://172.31.9.225:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+              println "Password = ${SONAR_PASS}"
+              sh "sonar-scanner -Dsonar.host.url=http://172.31.9.225:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+            }
           }
         }
-//      }
 
         stage('Upload code to Centralized Place') {
           steps {
@@ -43,7 +43,6 @@ def call() {
           }
         }
 
-//      }
       }
     }
   } catch(Exception e) {
