1ee51b364b64256ef4c7f42a3b4a1223c050ae00
Hari <harishsarma.v@gmail.com>
2023-02-18 18:21:22 +0530

    updating roboshop-jenkins-shared-library

--- vars/ci-declarative.groovy
+++ vars/ci-declarative.groovy
@@ -1,32 +1,51 @@
 def call() {
   try {
-    node('workstation') {
+    pipeline {
 
-      stage('CleanUp') {
-        cleanWs()
+      agent {
+        label 'workstation'
       }
 
-      stage('Compile/Build') {
-        common.compile()
-      }
+      stages {
 
-      stage('Unit Tests') {
-        common.unittests()
-      }
+        stage('Compile/Build') {
+          steps {
+            script {
+              common.compile()
+            }
+          }
+        }
 
-      stage('Quality Control') {
-        SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \\\'s/"//g\\\'', returnStdout: true).trim()
-        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-        wrap([$class: 'maskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-          sh "sonar-scanner -Dsonar.host.url=http://172.31.9.225:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+        stage('Unit Tests') {
+          steps {
+            script {
+              common.unittests()
+            }
+          }
+        }
+
+        stage('Quality Control') {
+          environment {
+            SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+          }
+          steps {
+            script {
+              SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \\\'s/"//g\\\'', returnStdout: true).trim()
+              wrap([$class: 'maskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                sh "sonar-scanner -Dsonar.host.url=http://172.31.9.225:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+              }
+            }
+          }
+        }
+
+
+        stage('Upload code to Centralized Place') {
+          steps {
+            echo 'upload'
+          }
         }
       }
-
-      stage('Upload code to Centralized Place') {
-        echo 'upload'
-      }
     }
-
   } catch(Exception e) {
     common.email("Failed")
   }
