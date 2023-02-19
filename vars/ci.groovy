188e59e5815dd5769432ad5e0b406828d2446ea8
Hari <harishsarma.v@gmail.com>
2023-02-19 22:19:01 +0530

    updating roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,9 +1,9 @@
 def call() {
-  if(!env.SONAR_EXTRA_OPTS) {
+  if (!env.SONAR_EXTRA_OPTS) {
     env.SONAR_EXTRA_OPTS = " "
   }
 
-  if(!env.TAG_NAME) {
+  if (!env.TAG_NAME) {
     env.PUSH_CODE = "false"
   } else {
     env.PUSH_CODE = "true"
@@ -27,21 +27,22 @@ def call() {
       }
 
       stage('Quality Control') {
-        SONAR_PASS = sh (script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-        SONAR_USER = sh (script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+        SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+        SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
         wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
 //          sh "sonar-scanner -Dsonar.host.url=http://172.31.9.225:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
           sh "echo Sonar Scan"
         }
       }
 
-      if(env.PUSH_CODE == "true") {
-      stage('Upload code to Centralized Place') {
-        echo 'upload'
+      if (env.PUSH_CODE == "true") {
+        stage('Upload code to Centralized Place') {
+          echo 'upload'
+        }
       }
     }
 
-  } catch(Exception e) {
-    common.email("Failed")
+  } catch (Exception e) {
+      common.email("Failed")
   }
 }
