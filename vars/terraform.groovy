c4e0110b2301ee05aadf14b565ce4f99cfcc1de0
Hari <harishsarma.v@gmail.com>
2023-03-03 18:28:49 +0530

    updating roboshop-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -25,8 +25,8 @@ def call() {
             stage('Terraform Apply') {
                 steps {
                     sh "terraform apply -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
+                }
+            }
         }
-
-
     }
 }
